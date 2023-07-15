d9fe12ccf41439cc8f62bf34b0b74ca6a525cd4c
aidan hopper <aidanhop1@gmail.com>
2023-07-14 18:38:05 -0700

    updating

--- init.lua
+++ init.lua
@@ -2,4 +2,3 @@ require("options")
 require("plugins")
 require("keys")
 require("telescope")
-
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -102,4 +102,25 @@ return require("packer").startup(function(use)
     requires = "nvim-tree/nvim-web-devicons"
   }
 
+  use {
+    "nvim-neorg/neorg",
+    config = function()
+        require('neorg').setup {
+            load = {
+                ["core.defaults"] = {}, -- Loads default behaviour
+                ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                ["core.dirman"] = { -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = "~/notes",
+                        },
+                    },
+                },
+            },
+        }
+    end,
+    run = ":Neorg sync-parsers",
+    requires = "nvim-lua/plenary.nvim",
+}
+
 end)
